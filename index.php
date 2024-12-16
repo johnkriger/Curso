7fb97914e2b538173cb6e84846861158c9b8962b
John Kryger <john.kryger.webart@gmail.com>
2024-12-16 15:07:27 -0300

    Breve explicação sobre o tipo de dados inteiro.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01 -</h3>
+                    <h3>Básico</h3>
                     <ul>
                         <li>
                             <a href="exercicio.php?dir=basico&file=ola">
@@ -35,6 +35,16 @@
                         <li>
                             <a href="exercicio.php?dir=basico&file=desafio">
                                Desafio PHP
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=inteiro">
+                                Tipo Inteiro
                             </a>
                         </li>
                     </ul>
