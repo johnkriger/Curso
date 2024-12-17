fe554af97f72cde0743696717dc71dcfa38c86ab
John Kryger <john.kryger.webart@gmail.com>
2024-12-17 10:30:32 -0300

    Inserindo informações sobre o float.

--- tipos/float.php
+++ tipos/float.php
@@ -3,4 +3,11 @@
 
 <?php
 echo 1.1, '<br>';
+var_dump(1.0);
+echo '<p><i>"Se for inserido um valor com casa decimal inválida (.0), mesmo assim o sistema vai interpretar como float.
+<br> Acima temos declarado o valor 1.0."</i></p><br>';
+echo PHP_FLOAT_MAX, '<p><i>"Função com o valor máximo armazenado pelo float."</i></p><br>';
+echo PHP_FLOAT_MIN, '<p><i>"Função com o valor mínimo armazenado pelo float."</i></p><br>';
+echo 1.2e3;
+echo 13E-3;
 ?>
