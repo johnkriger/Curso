e1deb50ba89da3044c607e9b20fc12f8e2fcd7b9
John Kryger <john.kryger.webart@gmail.com>
2024-12-18 13:41:45 -0300

    Mais funções.

--- tipos/string.php
+++ tipos/string.php
@@ -15,8 +15,10 @@ echo 'strtolower() COLOCA TODO O TEXTO EM MINÚSCULAS. ' . strtolower('COLOCA TO
 echo 'ucfirst() coloca a primeira letra de uma frase em maiúscula. ' . ucfirst('coloca a primeira letra de uma frase em maiúscula.<br>');
 echo 'ucwords() coloca a primeira letra de cada palavra em maiúscula. ' . ucwords('coloca a primeira letra de cada palavra em maiúscula.<br>');
 echo 'strlen() Conta quantos caracteres tem na string. (Para contar corretamente caracteres com acentos de ser usado o mb_strlen()) ';
-echo strlen('strlen() Conta quantos caracteres tem na string. (Para contar corretamente caracteres com acentos de ser usado o mb_strlen())'),'<br>';
-echo 'substr("Mostra apenas uma parte do texto informado.", 7, 7) ' . substr("Mostra apenas uma parte do texto informado.",7, 7);
+echo strlen('strlen() Conta quantos caracteres tem na string. (Para contar corretamente caracteres com acentos de ser usado o mb_strlen())') . '<br>';
+echo 'substr("Mostra apenas uma parte do texto informado.", 7, 7) ' . substr("Mostra apenas uma parte do texto informado.",7, 7) . '<br>';
 echo 'str_replace("definida", "informada", "Troca a palavra definida no primeiro parâmetro pela definida no segundo. Funciona em todas as ocorrências.")<br>';
 echo str_replace("definida", "informada", "Troca a palavra definida no primeiro parâmetro pela definida no segundo. Funciona em todas as ocorrências.");
+echo 'strpos("!AbcaBcabc", "abc") retorna a posição do item encontrado. Nesse caso será diferenciado maiúsculas e minúsculas. ' . strpos("!AbcaBcabc", "abc") . '<br>';
+echo 'stripos("!AbcaBcabc", "abc") retorna a posição do item encontrado. Nesse caso não será diferenciado maiúsculas e minúsculas. ' . stripos("!AbcaBcabc", "abc");
 ?>
