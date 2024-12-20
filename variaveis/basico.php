2697c85f80a675f0f8bf78277165be422a4efd83
John Kryger <john.kryger.webart@gmail.com>
2024-12-20 18:37:16 -0300

    Coisas básicas sobre variáveis.

---
+++ variaveis/basico.php
@@ -0,0 +1,9 @@
+<div class="titulo">Básico</div>
+
+<?php
+echo 'O PHP tem tipagem fraca para variáveis.<br>';
+echo 'Variáveis servem para o armazenamento de valores que podem ser pegos em um banco de dados, inseridos por um cliente, gerados pelo sistema etc.<br>';
+echo 'Atualmente a empresa em que eu trabalho utiliza o padrão Camel Case para a declaração de variáveis, onde ela é declarada com letras minúsculas e quando tem mais de uma palavra ela começa com maiúscula<br>';
+echo 'EX: $souVariavel <br>';
+echo 'Não é comum o uso de acentos em variáveis, embora a linguagem PHP permita isso.'
+?>
