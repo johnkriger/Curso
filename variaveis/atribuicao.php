d6f626a280051c3f2d7f94a3d81db92bc134a4e2
John Kryger <john.kryger.webart@gmail.com>
2024-12-21 10:54:42 -0300

    Atribuições.

---
+++ variaveis/atribuicao.php
@@ -0,0 +1,18 @@
+<div class="titulo">Atribuição</div>
+
+<?php
+echo 'Atribuição é o ato de definir um valor para uma variável e para isso é usado o sinal de igual.<br>';
+echo 'Podemos atribuir calculos, textos, numeros etc.<br>';
+echo 'EX: $variavel = 20<br>';
+echo '$texto = "texto"<br> $numero = 8<br> $calculo = 10 + 5<br> $calculoComVariaveis = $variavel + $numero<br>';
+$variavel = 20;
+$texto = "texto";
+$numero = 8;
+$calculo = 10 + 5;
+$calculoComVariaveis = $variavel + $numero;
+echo $variavel, '<br>';
+echo $texto, '<br>';
+echo $numero, '<br>';
+echo $calculo, '<br>';
+echo $calculoComVariaveis, '<br>';
+?>
