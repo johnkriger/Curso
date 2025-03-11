9f2867070be469d3848c410b10faca5e54cf6766
John Kryger <john.kryger.webart@gmail.com>
2025-03-11 20:27:31 -0300

    Includes.

---
+++ includes/arquivo_include.php
@@ -0,0 +1,11 @@
+<?php
+echo "Chamou-me??<br>";
+
+$var = 'Tô aqui caraio!!<br>';
+
+function soma($a, $b) {
+    return $a + $b . ' é meuzovo!<br>';
+}
+
+
+?>
---
+++ includes/include.php
@@ -0,0 +1,8 @@
+<div class="titulo">Include</div>
+
+<?php
+echo 'Executei! <br>';
+include('arquivo_include.php');
+echo soma(3, 8) . '<br>';
+echo $var;
+?>
